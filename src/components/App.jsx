00dc8d97927ea455f00b3294ae9d2990e357ea4b
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-05-14 14:59:45 +0300

    added changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+// The App component serves as the root component of the application, rendering the Header and PlantPage components.
 function App() {
   return (
     <div className="app">
